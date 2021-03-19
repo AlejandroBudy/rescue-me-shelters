fb2bddf67eb54dfe712883abf91d5af57594832b
AlejandroBudy <atorresato@gmail.com>
2021-03-19 19:09:35 +0100

    test implementation

---
+++ src/test/kotlin/org/rescueme/es/shelter/domain/view/create/ShelterViewCreatorTest.kt
@@ -0,0 +1,25 @@
+package org.rescueme.es.shelter.domain.view.create
+
+import io.kotest.core.spec.style.StringSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.rescueme.es.shelter.domain.view.ShelterViewRepository
+import org.rescueme.es.shelter.utils.createShelterView
+import org.rescueme.es.shelter.utils.shelterIdVO
+
+class ShelterViewCreatorTest : StringSpec({
+
+    val repository = mockk<ShelterViewRepository>()
+    val creator = ShelterViewCreator(repository)
+
+    "Should invoke repository to create shelter view"{
+
+        every { repository.create(createShelterView()) } returns Unit
+
+        creator.invoke(shelterIdVO)
+
+        verify(exactly = 1) { repository.create(createShelterView()) }
+    }
+
+})
